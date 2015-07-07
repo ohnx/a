8f7bdf57c7ac4cb8fdd492aaadb82f222e25f667
Mason X <masonx.01@gmail.com>
2015-07-07 18:38:29 -0400

    libbaremetal fixes

--- src/x86-64/syscalls/screen.asm
+++ src/x86-64/syscalls/screen.asm
@@ -1,10 +1,9 @@
 ; =============================================================================
 ; BareMetal -- a 64-bit OS written in Assembly for x86-64 systems
-; Copyright (C) 2008-2015 Return Infinity -- see LICENSE.TXT
+; Copyright (C) 2008-2014 Return Infinity -- see LICENSE.TXT
 ;
 ; Screen Output Functions
 ; =============================================================================
-
 
 ; -----------------------------------------------------------------------------
 ; os_inc_cursor -- Increment the cursor by one, scroll if needed
@@ -81,24 +80,12 @@ os_print_newline_done:
 ;  IN:	RSI = message location (zero-terminated string)
 ; OUT:	All registers preserved
 os_output:
-	push rdi
 	push rcx
-	push rax
 
-	xor ecx, ecx
-	xor eax, eax
-	mov rdi, rsi
-	not rcx
-	cld
-	repne scasb			; compare byte at RDI to value in AL
-	not rcx
-	dec rcx
-
+	call os_string_length
 	call os_output_chars
 
-	pop rax
 	pop rcx
-	pop rdi
 	ret
 ; -----------------------------------------------------------------------------
 
@@ -114,6 +101,10 @@ os_output_char:
 	push rbx
 	push rax
 
+	cmp byte [os_VideoEnabled], 1
+	je os_output_char_graphics
+
+os_output_char_text:
 	mov ah, 0x07			; Store the attribute into AH so STOSW can be used later on
 
 	push rax
@@ -133,6 +124,11 @@ os_output_char:
 	add rdi, rbx
 	stosw				; Write the character and attribute to screen
 
+	jmp os_output_char_done
+
+os_output_char_graphics:
+	call os_glyph_put
+
 os_output_char_done:
 	call os_inc_cursor
 
@@ -146,8 +142,144 @@ os_output_char_done:
 
 
 ; -----------------------------------------------------------------------------
+; os_pixel -- Put a pixel on the screen
+;  IN:	EBX = Packed X & Y coordinates (YYYYXXXX)
+;	EAX = Pixel Details (AARRGGBB)
+; OUT:	All registers preserved
+os_pixel:
+	push rdi
+	push rdx
+	push rcx
+	push rbx
+	push rax
+
+	push rax			; Save the pixel details
+	mov rax, rbx
+	shr eax, 16			; Isolate Y co-ordinate
+	xor ecx, ecx
+	mov cx, [os_VideoX]
+	mul ecx				; Multiply Y by os_VideoX
+	and ebx, 0x0000FFFF		; Isolate X co-ordinate
+	add eax, ebx			; Add X
+	mov rdi, [os_VideoBase]
+
+	cmp byte [os_VideoDepth], 32
+	je os_pixel_32
+
+os_pixel_24:
+	mov ecx, 3
+	mul ecx				; Multiply by 3 as each pixel is 3 bytes
+	add rdi, rax			; Add offset to pixel video memory
+	pop rax				; Restore pixel details
+	stosb
+	shr eax, 8
+	stosb
+	shr eax, 8
+	stosb
+	jmp os_pixel_done
+
+os_pixel_32:
+	shl eax, 2			; Quickly multiply by 4
+	add rdi, rax			; Add offset to pixel video memory
+	pop rax				; Restore pixel details
+	stosd
+
+os_pixel_done:
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdx
+	pop rdi
+	ret
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; os_glyph_put -- Put a glyph on the screen at the cursor location
+;  IN:	AL  = char to display
+; OUT:	All registers preserved
+os_glyph_put:
+	push rdi
+	push rsi
+	push rdx
+	push rcx
+	push rbx
+	push rax
+
+	and eax, 0x000000FF
+	sub rax, 0x20
+	mov ecx, 12			; Font height
+	mul ecx
+	mov rsi, font_data
+	add rsi, rax			; add offset to correct glyph
+
+; Calculate pixel co-ordinates for character
+	xor ebx, ebx
+	xor edx, edx
+	xor eax, eax
+	mov ax, [os_Screen_Cursor_Row]
+	mov cx, 12			; Font height
+	mul cx
+	mov bx, ax
+	shl ebx, 16
+	xor edx, edx
+	xor eax, eax
+	mov ax, [os_Screen_Cursor_Col]
+	mov cx, 6			; Font width
+	mul cx
+	mov bx, ax
+
+	xor eax, eax
+	xor ecx, ecx			; x counter
+	xor edx, edx			; y counter
+
+nextline1:
+	lodsb				; Load a line
+
+nextpixel:
+	cmp ecx, 6			; Font width
+	je bailout			; Glyph row complete
+	rol al, 1
+	bt ax, 0
+	jc os_glyph_put_pixel
+	push rax
+	mov eax, 0x00000000
+	call os_pixel
+	pop rax
+	jmp os_glyph_put_skip
+
+os_glyph_put_pixel:
+	push rax
+	mov eax, [os_Font_Color]
+	call os_pixel
+	pop rax
+os_glyph_put_skip:
+	add ebx, 1
+	add ecx, 1
+	jmp nextpixel
+
+bailout:
+	xor ecx, ecx
+	sub ebx, 6			; column start
+	add ebx, 0x00010000		; next row
+	add edx, 1
+	cmp edx, 12			; Font height
+	jne nextline1
+
+glyph_done:
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdx
+	pop rsi
+	pop rdi
+	ret
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
 ; os_output_chars -- Displays text
-;  IN:	RSI = message location (A string, not zero-terminated)
+; IN:	RSI = message location (A string, not zero-terminated)
 ;	RCX = number of chars to print
 ; OUT:	All registers preserved
 os_output_chars:
@@ -231,6 +363,10 @@ os_screen_scroll:
 
 	xor ecx, ecx
 
+	cmp byte [os_VideoEnabled], 1
+	je os_screen_scroll_graphics
+
+os_screen_scroll_text:
 	mov rsi, os_screen 		; Start of video text memory for row 2
 	add rsi, 0xA0
 	mov rdi, os_screen 		; Start of video text memory for row 1
@@ -242,6 +378,15 @@ os_screen_scroll:
 	mov cx, 80
 	rep stosw			; Store word in AX to RDI, RCX times
 	call os_screen_update
+	jmp os_screen_scroll_done
+
+os_screen_scroll_graphics:
+	xor esi, esi
+	mov rdi, [os_VideoBase]
+	mov esi, [os_Screen_Row_2]
+	add rsi, rdi
+	mov ecx, [os_Screen_Bytes]
+	rep movsb
 
 os_screen_scroll_done:
 	popfq
@@ -267,11 +412,22 @@ os_screen_clear:
 
 	xor ecx, ecx
 
+	cmp byte [os_VideoEnabled], 1
+	je os_screen_clear_graphics
+
+os_screen_clear_text:
 	mov ax, 0x0720			; 0x07 for black background/white foreground, 0x20 for space (black) character
 	mov rdi, os_screen		; Address for start of frame buffer
 	mov cx, 2000			; 80 x 25
 	rep stosw			; Clear the screen. Store word in AX to RDI, RCX times
 	call os_screen_update
+	jmp os_screen_clear_done
+
+os_screen_clear_graphics:
+	mov rdi, [os_VideoBase]
+	xor eax, eax
+	mov ecx, [os_Screen_Bytes]
+	rep stosb
 
 os_screen_clear_done:
 	popfq
