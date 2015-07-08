b13170ad3f52b6fd04e89e50a9433acb3cb99c7d
Mason X <ohnx@users.noreply.github.com>
2015-07-08 13:44:26 -0400

    Create graphics.asm

---
+++ src/x86-64/graphics.asm
@@ -0,0 +1,8 @@
+; =============================================================================
+; BareMetal -- a 64-bit OS written in Assembly for x86-64 systems
+; ohnx was here
+; Graphics includes
+; =============================================================================
+
+%include "graphics/font.asm"
+%include "graphics/basic.asm"
